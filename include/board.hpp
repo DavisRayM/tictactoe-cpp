b576026ddb52c24de455a4eda67c020997622aae
Davis Muro <git@davisraym.com>
2025-11-04 02:11:50 -0800

    feat: update board tracking functionality

--- include/board.hpp
+++ include/board.hpp
@@ -1,17 +1,42 @@
+#include <vector>
+
 enum Token {
   CIRCLE=1,
   X,
 };
 
+enum BoardWinState {
+  ONGOING = 0,
+  CIRCLE_WIN = 1,
+  X_WIN = 2,
+  DRAW = 3,
+  ERROR = 4,
+};
+
+struct Move {
+  int x;
+  int y;
+
+  Move(int x, int y) {
+    this->x = x;
+    this->y = y;
+  }
+};
+
 struct Board {
   Token state[3][3];
 
-  Board();
-  ~Board();
-
-  /// Updates the board state at position (x, y) to show the current `player`.
-  /// Returns the new generation of the board with the updated state.
+  /// Simulates the player move based on current board state.
+  /// Updates the memory stored at `board`. Returns a boolean on whether process
+  /// was succesful.
   ///
   /// pre: state[x][y] is not set.
-  int play(int x, int y, Token player, Board *board);
+  bool Play(int x, int y, Token player, Board *board) const;
+
+  /// Returns the current win state of the board.
+  BoardWinState WinState() const;
+
+  /// Populates `moves` with the current valid moves. Previous pointer is not
+  /// cleaned up as such implementation should handle.
+  std::vector<Move> ValidMoves() const;
 };
--- include/game.hpp
+++ include/game.hpp
@@ -6,8 +6,38 @@ public:
   ~Game();
 
   /// Display the current game board.
-  bool display();
+  void Display() const;
+
+  /// Updates the current game generation; requests a move
+  /// from the player and transitions to next state. Returns whether
+  /// the game is still live.
+  ///
+  /// pre: live is true
+  /// post: updates generation and live
+  bool Update();
+
 private:
-  Board current;
+  Board *current;
   Token player;
+  unsigned int generation;
+  bool live;
+
+  /// Updates current board generation.
+  ///
+  /// pre: move is a valid move for the current generation
+  BoardWinState UpdateBoard(Move move) {
+    Board *next = new Board;
+    if (!current->Play(move.x, move.y, player, next))
+      return ERROR;
+
+    delete current;
+    current = next;
+
+    if (player == X)
+      player = CIRCLE;
+    else
+      player = X;
+
+    return current->WinState();
+  }
 };
