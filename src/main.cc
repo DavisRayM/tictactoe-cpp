bde6fba5296af3e6472533214879722ae58636d6
Davis Muro <git@davisraym.com>
2025-11-04 12:30:25 -0800

    feat: simulate entire games with AI

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
 #include <game.hpp>
+#include <ai.hpp>
 
 int main(int argc, char** argv) {
   Game game;
 
+  // Fully AI Game
   game.Display();
-  while (game.Update()) {
+  while (game.Update(true)) {
     game.Display();
   }
   return 0;
