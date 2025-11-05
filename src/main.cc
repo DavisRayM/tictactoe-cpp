bd12995c4402cb956f4231897086e0bb0bec7c57
Davis Muro <git@davisraym.com>
2025-11-04 17:46:36 -0800

    feat: handle user interaction in binary

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,57 @@
 #include <ai.hpp>
 #include <game.hpp>
+#include <iostream>
+
+Move RequestMove(Game &game) {
+  Move move(-1, -1);
+  std::cout << "Enter x coordinate: ";
+  std::cin >> move.x;
+
+  std::cout << "Enter y coordinate: ";
+  std::cin >> move.y;
+
+  while (!game.IsValidMove(move)) {
+    std::cout << "invalid coordinates.\n\n";
+
+    std::cout << "Enter x coordinate: ";
+    std::cin >> move.x;
+
+    std::cout << "Enter y coordinate: ";
+    std::cin >> move.y;
+  }
+
+  return move;
+}
+
+bool Update(Game &game, bool simulate) {
+  Move move(-1, -1);
+  bool showBoard = false;
+
+  if (simulate)
+    move = BestMove(game.CurrentBoard(), game.PlayerToken());
+  else
+    move = RequestMove(game);
+
+  BoardWinState state = game.UpdateBoard(move);
+
+  if (state == CIRCLE_WIN) {
+    showBoard = true;
+    std::cout << "O wins!\n";
+  }
+  if (state == X_WIN) {
+    showBoard = true;
+    std::cout << "X wins!\n";
+  }
+  if (state == DRAW) {
+    showBoard = true;
+    std::cout << "No one wins!\n";
+  }
+
+  if (showBoard)
+    std::cout << game.Display();
+
+  return game.IsLive();
+}
 
 int main(int argc, char **argv) {
   Game game;
@@ -7,12 +59,11 @@ int main(int argc, char **argv) {
   bool simulate = false;
 
   // vs AI Game
-  game.Display();
-  do {
-    gameOngoing = game.Update(simulate);
-    simulate = !simulate;
-    game.Display();
-  } while (gameOngoing);
+  while (game.IsLive()) {
+    std::cout << "\nPlayer " << game.PlayerDisplay() << "'s turn.\n";
+    std::cout << game.Display();
 
+    Update(game, simulate);
+  }
   return 0;
 }
